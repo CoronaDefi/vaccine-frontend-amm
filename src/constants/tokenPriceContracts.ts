293fba2e4ffe1a7288a4f2a9a04a3b280b173d09
baguette <baguette@finance>
2021-03-15 15:25:00 +0100

    Full code

---
+++ src/constants/tokenPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x8ef51a58Bfa952dF837c7D4810224f7De992ebA5',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xeaab1d6ff7ed1bca766bf66f70f7e5fc1a0005d7'
+}
+
+export default priceContracts
